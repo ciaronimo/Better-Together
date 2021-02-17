eb874bca96dad947a17f02499fc93a23da44edd5
terishrimp <72040129+terishrimp@users.noreply.github.com>
2021-02-17 15:17:55 -0500

    PlayerAttack Update
    used booleans for each attack instead of triggers
    
    used animation exit times
    
    created atk cooldown so player can't spam

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -14,13 +14,19 @@ public class PlayerAttack : MonoBehaviour
     public Animator anim;
     public int atkType = 0;
     public float timerPeriod = .25f;
+    public float atkCdTimerPeriod = .25f;
 
     bool timerOn;
     bool isAttacking = false;
+    bool attack1Active = false;
+    bool attack2Active = false;
+    bool attack3Active = false;
     float timer = 0f;
+    float atkCdTimer;
     // Start is called before the first frame update
     void Start()
     {
+        atkCdTimer = atkCdTimerPeriod;
         anim = GetComponent<Animator>();
     }
 
@@ -28,65 +34,62 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl) && atkCdTimer >= atkCdTimerPeriod)
         {
             timerOn = true;
             isAttacking = true;
             timer = 0;
             atkType++;
-            switch (atkType)
-            {
-                case 1:
-                    anim.SetTrigger("attack1");
-                    break;
-                case 2:
-                    anim.SetTrigger("attack1");
-                    anim.SetTrigger("attack2");
-                    break;
-                case 3:
-                    anim.SetTrigger("attack1");
-                    anim.SetTrigger("attack2");
-                    anim.SetTrigger("attack3"); 
-                    Debug.Log("attack3");
-                    break;
-                default:
-
-                    break;
-
-            }
             Debug.Log(atkType);
-            //anim.SetTrigger("attack1");
-            //camAnim.SetTrigger("Shake");
+        }
 
 
-            //Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-            /*
-            for (int i = 0; i < enemiesToDamage; i++)
-            {
-                enemiesToDamage[i].GetComponent<Enemy>().health -= damage;      
-            */
+        switch (atkType)
+        {
+            case 1:
+                attack1Active = true;
+                break;
+            case 2:
+                attack2Active = true;
+                break;
+            case 3:
+                attack3Active = true;
+                Debug.Log("attack3");
+                break;
+            default:
+
+                break;
 
         }
 
-        //if (Input.GetKeyUp(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.LeftControl))
-        //{
-        //    isAttacking = false;
-        //}
-            if (timer >= timerPeriod)
+        if (timer >= anim.GetCurrentAnimatorStateInfo(0).length && isAttacking)
         {
+            atkCdTimer = 0;
             isAttacking = false;
             timerOn = false;
+
         }
 
         if (timerOn) timer += Time.deltaTime;
 
-        if (atkType > 3 || timer > timerPeriod)
+        if(atkCdTimer < atkCdTimerPeriod)
         {
-            atkType = 0;
+            atkCdTimer += Time.deltaTime;
         }
 
-        //timeBtwAttack -= Time.deltaTime;
+        if (atkType > 3 || timer >= anim.GetCurrentAnimatorStateInfo(0).length)
+        {
+
+            atkType = 0;
+            attack1Active = false;
+            attack2Active = false;
+            attack3Active = false;
+        }
+
         anim.SetBool("isAttacking", isAttacking);
+        anim.SetBool("attack1Active", attack1Active);
+        anim.SetBool("attack2Active", attack2Active);
+        anim.SetBool("attack3Active", attack3Active);
     }
 
     private void OnDrawGizmosSelected()
